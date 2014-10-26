eacc4cbcfd61636b1dc35e49df1bb492371fc39b
Humberto Gloor Campos <hgloor@gmail.com>
2014-10-26 20:26:39 -0200

    Programming Assignment 2 - Resolution
    Functions to store and calculate the inverse of a matrix, storing the
    result into a kind of cache

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## functions to store and calculate the inverse of a matrix, storing 
+## the result into a kind of cache
 
-## Write a short comment describing this function
+## makeCacheMatrix: function to store and show a matrix and your inverse 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	i <- NULL
+	set <- function(y) {
+		x <<- y
+		i <<- NULL
+	}
+	get <- function() x
+	setinverse <- function(solve) i <<- solve
+	getinverse <- function() i
+	list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve: function to calculate the inverse of a matrix stored 
+## in a "makeCacheMatrix" object
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	## Return the inverse matrix of 'x'
+	i <- x$getinverse()
+	if(!is.null(i)) {
+		message("getting cached data")
+		return(i)
+	}
+	i <- solve(x$get(), ...)
+	x$setinverse(i)
+	i
 }
